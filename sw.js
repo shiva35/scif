42b67bf66f40f62fc4996b411469a61a5e70ad31
shiva35 <behappy.shiva@gmail.com>
2025-10-14 21:13:50 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,9 +2,9 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('scif-v1').then(function(cache) {
       return cache.addAll([
-        '/',
-        '/index.html',
-        '/manifest.json'
+        './',
+        './index.html',
+        './manifest.json'
       ]);
     })
   );
